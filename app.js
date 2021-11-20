d0e7029ff655c6b8c50973419c45580d6645b795
nazar301 <nazar.basistyy@gmail.com>
2021-11-19 21:24:37 -0500

    buttons established

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-
-// let moves = document.querySelector('.moves').val
-
-// const selectTopDisc = document.querySelector(".box.selected") 
-
-  //  let discSelector = document.querySelectorAll(".box").addEventListener("click" (
-  //   discSelector.onclick = function(){
-  //     select.target.pop(":nth-child") 
-  //     console.log("here")
-  //   }
-  
-
-    // discSelector.onclick = function () {
-    //   slect.target.append ()
-    // }
-  
-  
-    
-    
-//     click on the next pole to change the location
-
-    
-// block out movement so bottom disk cant be moved if it has something on top
-// block stacking to prevent movement of bigger disk over small disk
-// event listeners for the disk
-// set a difficulty from 3 to 10 disks disable the others depending on choice
-
-
-
-// drag function /movement
-
+// button selecttion for 3 disks
 let buttonThree = document.querySelector(".BSelectThree").addEventListener("click", () => {
   document.getElementById("one").style.display="initial";
   document.getElementById("two").style.display="initial";
@@ -43,84 +13,84 @@ let buttonThree = document.querySelector(".BSelectThree").addEventListener("clic
 
   
 });
-// let buttonFive = document.querySelector(".BSelectFive").addEventListener("click", () => {
-//   document.getElementById("one").style.display="initial";
-//   document.getElementById("two").style.display="initial";
-//   document.getElementById("three").style.display="initial";
-//   document.getElementById("four").style.display="initial";
-//   document.getElementById("five").style.display="initial";
-//   document.getElementById("six").style.display="none";
-//   document.getElementById("seven").style.display="none";
-//   document.getElementById("eight").style.display="none";
-//   document.getElementById("nine").style.display="none";
-//   document.getElementById("ten").style.display="none";
-// });
-// let buttonTen = document.querySelector(".BSelectTen").addEventListener("click", () => {
-//   document.getElementById("one").style.display="initial";
-//   document.getElementById("two").style.display="initial";
-//   document.getElementById("three").style.display="initial";
-//   document.getElementById("four").style.display="initial";
-//   document.getElementById("five").style.display="initial";
-//   document.getElementById("six").style.display="initial";
-//   document.getElementById("seven").style.display="initial";
-//   document.getElementById("eight").style.display="initial";
-//   document.getElementById("nine").style.display="initial";
-//   document.getElementById("ten").style.display="initial";
-// });
-let box = document.querySelector(".box")
+// button selection for 5 disk
+let buttonFive = document.querySelector(".BSelectFive").addEventListener("click", () => {
+  document.getElementById("one").style.display="initial";
+  document.getElementById("two").style.display="initial";
+  document.getElementById("three").style.display="initial";
+  document.getElementById("four").style.display="initial";
+  document.getElementById("five").style.display="initial";
+  document.getElementById("six").style.display="none";
+  document.getElementById("seven").style.display="none";
+  document.getElementById("eight").style.display="none";
+  document.getElementById("nine").style.display="none";
+  document.getElementById("ten").style.display="none";
+});
+// button selection for all
+let buttonTen = document.querySelector(".BSelectTen").addEventListener("click", () => {
+  document.getElementById("one").style.display="initial";
+  document.getElementById("two").style.display="initial";
+  document.getElementById("three").style.display="initial";
+  document.getElementById("four").style.display="initial";
+  document.getElementById("five").style.display="initial";
+  document.getElementById("six").style.display="initial";
+  document.getElementById("seven").style.display="initial";
+  document.getElementById("eight").style.display="initial";
+  document.getElementById("nine").style.display="initial";
+  document.getElementById("ten").style.display="initial";
+});
+
+// containers for disks 
+let boxLeft = document.querySelector("#left")
+let boxCenter = document.querySelector("#center")
+let boxRight = document.querySelector("#right")
+
+
+
+// drag and drop function
 function allowDrop(ev) {
   ev.preventDefault();
+  
 }
 
 function drag(ev) {
-  let target = ev.target
   ev.dataTransfer.setData("text", ev.target.id);
-  console.log(ev.dataTransfer)
-  if(target != [0]) {
-    // stop drag 
-    // console.log(target)
-  }
-  // if(box.length > 1){
-  //   target.disk[0]
-  //   console.log(target.disk[0])
-  // }; 
+  
+ 
+  if (boxLeft.hasChildNodes()) {
+    let children = boxLeft.childNodes;
+  
+    for (let i = 0; i < children.length; i++) {
+      // console.log(boxLeft.childNodes)
 
+}; 
 }
+  }
+  
+  if (boxCenter.hasChildNodes()) {
+    let children = boxCenter.childNodes;
+  
+    for (let i = 0; i < children.length; i++) {
+      // console.log(boxCenter.childNodes)
+      
+}
+  };
+  if (boxRight.hasChildNodes()) {
+    let children = boxRight.childNodes;
+  
+    for (let i = 0; i < children.length; i++) {
+      // console.log(boxRight.childNodes)
+      
+}
+  };
 
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+  ev.target.prepend(document.getElementById(data));
   // console.log(ev.target)
   
 }
-
-
-let boxSample = document.getElementsByClassName('box');
-child = boxSample.children;
-// console.log(child)
-
-
-// const movables = document.querySelectorAll(".disk")
-// const container = document.querySelectorAll(".box")
-// movables.forEach(movable => { 
-//   console.log(movable)
-//   movable.addEventListener('dragstart', () => {
-//  console.log('moved')
-//   }),
-//   movable.addEventListener('dragend', () => {
-//     console.log('stoped')
-//   })
-// })
-
-// container.forEach(container => {
-//    container.addEventListener('dragover', e => {
-//      e.preventDefault()
-     
-//     //  const movable = document.querySelector(".disk")
-//      container.prepend(movables)
-//    })
-// })
 
 
 
